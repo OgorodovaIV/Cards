e8fc07555bc09e75aba655bd3859f73d3016fbdf
OgorodovaIV <128236944+OgorodovaIV@users.noreply.github.com>
2023-03-18 23:02:56 +0400

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -4,7 +4,7 @@ public class Cards {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in, "cp866");
+        Scanner scanner = new Scanner(System.in);
         String comand;
         String[] koloda = new String[] {
             "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10",
@@ -20,50 +20,46 @@ public class Cards {
         while (true) {
             System.out.println("Введите команду (exit для выхода): ");
             comand = scanner.next();
-            switch (comand) {
-                case "exit" -> {
-                    break OUTER;
-                }
-                case "start" -> {
-                    c = scanner.nextInt();
-                    n = scanner.nextInt();
-                    if (c * n > koloda.length) {
-                        System.out.println("Ошибка: в колоде недостаточно карт для раздачи");
-                    } else {
-                        // Перемешать колоду
-                        for (int i = 0; i < koloda.length; i++) {
-                            razdacha[i] = koloda[(int) (Math.random() * koloda.length)];
-                            // Проверим - не переносилась ли в раздачу уже эта карта?
-                            for (int j = 0; j < i; j++) {
-                                if (razdacha[j].equals(razdacha[i])) {
-                                    // Да, карата уже переносилась, попытаемся выбрать другую карту
-                                    i--;
-                                    break;
-                                }
-                            }
-                        }
-                        // Раздарим из перемешанной колоды
-                        int k = 0;
-                        igroki = new String[c];
-                        for (int i = 0; i < c; i++) {
-                            igroki[i] = "";
-                            for (int j = 1; j <= n; j++) {
-                                igroki[i] = igroki[i] + " " + razdacha[k++];
+            if (comand.equals("exit")) {
+                break OUTER;
+            } else if (comand.equals("start")) {
+                c = scanner.nextInt();
+                n = scanner.nextInt();
+                if (c * n > koloda.length) {
+                    System.out.println("Ошибка: в колоде недостаточно карт для раздачи");
+                } else {
+                    // Перемешать колоду
+                    for (int i = 0; i < koloda.length; i++) {
+                        razdacha[i] = koloda[(int) (Math.random() * koloda.length)];
+                        // Проверим - не переносилась ли в раздачу уже эта карта?
+                        for (int j = 0; j < i; j++) {
+                            if (razdacha[j].equals(razdacha[i])) {
+                                // Да, карата уже переносилась, попытаемся выбрать другую карту
+                                i--;
+                                break;
                             }
                         }
                     }
-                }
-                case "get-cards" -> {
-                    c = scanner.nextInt();
-                    if (c > igroki.length) {
-                        System.out.println("Ошибка: номер игрока больше количества игроков");
-                    } else {
-                        System.out.println(c + igroki[c - 1]);
+                    // Раздарим из перемешанной колоды
+                    int k = 0;
+                    igroki = new String[c];
+                    for (int i = 0; i < c; i++) {
+                        igroki[i] = "";
+                        for (int j = 1; j <= n; j++) {
+                            igroki[i] = igroki[i] + " " + razdacha[k++];
+                        }
                     }
                 }
-                default -> System.out.println("Ошибка: Неизвестная команда");
+            } else if (comand.equals("get-cards")) {
+                c = scanner.nextInt();
+                if (c > igroki.length) {
+                    System.out.println("Ошибка: номер игрока больше количества игроков");
+                } else {
+                    System.out.println(c + igroki[c - 1]);
+                }
+            } else {
+                System.out.println("Ошибка: Неизвестная команда");
             }
         }
     }
-    
 }
